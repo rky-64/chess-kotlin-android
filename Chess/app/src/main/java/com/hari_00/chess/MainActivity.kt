654166fb54a61b53e98ca1a4e38aabd22665a721
rky-64 <rajnishkumar57813@gmail.com>
2022-04-16 21:38:02 +0530

    created Android Project for chess

---
+++ Chess/app/src/main/java/com/hari_00/chess/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.hari_00.chess
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
